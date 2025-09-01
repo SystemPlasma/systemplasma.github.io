4b8bfb7989c7df256c034918328c4ac871fce129
GetSirius55 <systemplasma13@gmail.com>
2025-09-01 16:24:13 -0500

    Updates

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Use leading and trailing slashes for correct asset paths on GitHub Pages
-  base: "/wkw-deckbuilder/"
-})
