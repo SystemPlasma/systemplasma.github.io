9cf6bf687b3bce1a13b4bd5b7d44dde4f4ae756e
GetSirius55 <systemplasma13@gmail.com>
2025-09-02 08:44:28 -0500

    Revert data

---
+++ src/src/imageMap.ts
@@ -0,0 +1,4 @@
+// Synced shim: re-export the generated map from one canonical location.
+import MAP, { CARD_IMAGE_URLS } from '../imageMap';
+export { CARD_IMAGE_URLS };
+export default MAP;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="vite/client" />
 
+// Allow importing raw CSV files via `?raw`
+declare module '*.csv?raw' {
+  const content: string;
+  export default content;
+}
+
+// Image modules not needed when using `new URL(..., import.meta.url)`
