62f0ba43b7b02dfd664c7af48a65627d6de66c56
GetSirius55 <systemplasma13@gmail.com>
2025-09-02 11:57:47 -0500

    Deploy /wkw-deckbuilder (single page)

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-// Allow importing raw CSV files via `?raw`
-declare module '*.csv?raw' {
-  const content: string;
-  export default content;
-}
-
-// Image modules not needed when using `new URL(..., import.meta.url)`
